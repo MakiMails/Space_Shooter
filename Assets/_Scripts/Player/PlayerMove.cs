76ce1bf1f8cf54ecc10feaf74ff12e3ab316af72
MakiMails <salivonhik1990@gmail.com>
2023-09-13 20:35:04 +0300

    Player Move

--- Assets/_Scripts/Player/PlayerMove.cs
+++ Assets/_Scripts/Player/PlayerMove.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
+using Game.States.PlayerState;
 
 namespace Game.PlayerScripts
 {
     internal class PlayerMove : MonoBehaviour
     {
-        [SerializeField] private float _speed;
-        [SerializeField] private Transform _cameraTransform;
-        private PlayerControllerAnim _playerControllerAnim;
+
+        [SerializeField] private PlayerMoveState _playerMoveStateDeffult;
+        [SerializeField] private PlayerMoveState _playerMoveStateAttack;
+
+        private float _speed;
+
+        public float Speed
+        {
+            get => _speed;
+            set => _speed = value;
+        }
 
         [SerializeField] private AnimationCurve _animationCurve;
+        private PlayerControllerAnim _playerControllerAnim;
+
+        private float _timeGainCoefficient = 6f;
+        private float _timeReductionCoefficient = 8f;
 
         private float _timeY;
         private float _timeX;
 
+        [SerializeField] private Transform _cameraTransform;
+
         private void Start()
         {
             _playerControllerAnim = GetComponent<PlayerControllerAnim>();
+            _speed = _playerMoveStateDeffult.Speed;
+            AddMirrorKeysInAnimationCurve();
         }
 
         public void Move()
@@ -25,49 +42,58 @@ namespace Game.PlayerScripts
 
             if (Input.GetKey(KeyCode.W))
             {
-                _timeY += Time.deltaTime * 2f;
-                _timeY = Mathf.Clamp(_timeY, 0f, 1f);
-
-                transform.position += cameraDirection * _speed * _animationCurve.Evaluate(_timeY) * Time.deltaTime;
+                _timeY = Mathf.Lerp(_timeY, 1f, Time.deltaTime * _timeGainCoefficient);
                 TurnBody(_cameraTransform.rotation);
-                _playerControllerAnim.Move(_animationCurve.Evaluate(_timeY));
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                _timeY -= Time.deltaTime * 2f;
-                _timeY = Mathf.Clamp(_timeY, -1f, 0f);
-
-                transform.position -= cameraDirection * _speed * _animationCurve.Evaluate(_timeY * -1f) * Time.deltaTime;
-                _playerControllerAnim.Move(-_animationCurve.Evaluate(_timeY * -1f));
+                _timeY = Mathf.Lerp(_timeY, -1f, Time.deltaTime * _timeGainCoefficient);
+                TurnBody(_cameraTransform.rotation);
             }
             else
             {
-                _timeY = Mathf.Lerp(_timeY, 0, Time.deltaTime * 2f);
-                _playerControllerAnim.Move(_timeY);
+                _timeY = Mathf.Lerp(_timeY, 0, Time.deltaTime * _timeReductionCoefficient);
             }
+
+            transform.position += cameraDirection * _speed * _animationCurve.Evaluate(_timeY) * Time.deltaTime;
+            _playerControllerAnim.Move(_animationCurve.Evaluate(_timeY));
 
             Vector3 directionLeft = Vector3.Cross(cameraDirection, Vector3.up);
 
             if (Input.GetKey(KeyCode.A))
             {
-                _timeX -= Time.deltaTime * 4f;
-                _timeX = Mathf.Clamp(_timeX, -1f, 0f);
-
-                transform.position += directionLeft * _speed * _animationCurve.Evaluate(_timeX * -1f) * Time.deltaTime;
-                _playerControllerAnim.Turn(-_animationCurve.Evaluate(_timeX * -1f));
+                _timeX = Mathf.Lerp(_timeX, 1f, Time.deltaTime * _timeGainCoefficient);
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                _timeX += Time.deltaTime * 4f;
-                _timeX = Mathf.Clamp(_timeX, 0f, 1f);
-
-                transform.position -= directionLeft * _speed * _animationCurve.Evaluate(_timeX) * Time.deltaTime;
-                _playerControllerAnim.Turn(_animationCurve.Evaluate(_timeX));
+                _timeX = Mathf.Lerp(_timeX, -1f, Time.deltaTime * _timeGainCoefficient);
             }
             else
             {
-                _timeX = Mathf.Lerp(_timeX, 0, Time.deltaTime * 4f);
-                _playerControllerAnim.Turn(_timeX);
+                _timeX = Mathf.Lerp(_timeX, 0, Time.deltaTime * _timeReductionCoefficient);
+            }
+
+            transform.position += directionLeft * _speed * _animationCurve.Evaluate(_timeX) * Time.deltaTime;
+            _playerControllerAnim.Turn(-_animationCurve.Evaluate(_animationCurve.Evaluate(_timeX)));
+        }
+
+        public void LowerSpeed()
+        {
+            Speed = _playerMoveStateAttack.Speed;
+        }
+
+        public void IncreaseSpeed()
+        {
+            Speed = _playerMoveStateDeffult.Speed;
+        }
+
+        private void AddMirrorKeysInAnimationCurve()
+        {
+            Keyframe[] keyframes = _animationCurve.keys;
+
+            for (int i = keyframes.Length - 1; i > -1; i--)
+            {
+                _animationCurve.AddKey(-keyframes[i].time, -keyframes[i].value);
             }
         }
 
