e85dba6b41b572602b7dc2ded721fb897474a60a
MakiMails <salivonhik1990@gmail.com>
2023-09-14 22:32:15 +0300

    Weapen

---
+++ Assets/_Scripts/Weapon/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Game.WeaponScripts
+{
+    internal class Weapon : MonoBehaviour
+    {
+        [SerializeField] private float _damage;
+
+        public void Shot()
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10 * hit.distance, Color.green, 1f);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red, 1f);
+            }
+        }
+
+    }
+}
